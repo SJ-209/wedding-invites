e5a58a9c90dbb8b947b681659f3804a83c66ecf8
Quake <142627174+SJ-209@users.noreply.github.com>
2026-08-15 14:34:56 +0100

    wedding invite images added

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,15 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-white">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-white sm:items-start">
         <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
+          className="drop-shadow-lg rounded-lg"
+          src="/images/invite-front.JPG"
+          alt="Wedding Invitation Front"
+          width={5000}
+          height={2500}
           priority
+        />
+        <Image
+          src="/images/invite-back.JPG"
+          alt="Wedding Invitation Back"
+          width={5000}
+          height={2500}
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
